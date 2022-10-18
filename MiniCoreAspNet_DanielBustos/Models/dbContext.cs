c19655b2a75c7b3fb3ecae89a49894260666860f
Daniel Bustos <daniel.bustos.velez@udla.edu.ec>
2022-10-18 11:18:28 -0500

    Agregar archivos de proyecto.

---
+++ MiniCoreAspNet_DanielBustos/Models/dbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniCoreAspNet_DanielBustos.Models
+{
+    public class dbContext : DbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Pase> Pases { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
